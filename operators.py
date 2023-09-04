eeb2257921d4e4649e5ea36bfc00e392f0ebc631
KanakantiTejeswarReddy <143984137+KanakantiTejeswarReddy@users.noreply.github.com>
2023-09-04 09:23:53 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+my_name = input("enter your name")
+print(my_name)
+
+
+# In[5]:
+
+
+a = 20
+b = 40
+c = a + b
+print(c)
+
+
+# In[6]:
+
+
+a = 20
+b = 10
+result = a - b
+print(result)
+
+
+# In[7]:
+
+
+a = 20
+b = 50
+result = a * b
+print(result)
+
+
+# In[8]:
+
+
+a = 40
+b = 4
+result = a / b
+print(result)
+
+
+# In[12]:
+
+
+a = 10
+b = 20
+sum = a + b
+print("the sum of two numbers is:",sum)
+
+
+# In[ ]:
+
+
+my_name = input("teja")
+print(my_name)
+
+
+# In[ ]:
+
+
+pi = 3.14
+print("the value of pi",pi)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
